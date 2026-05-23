53278f046fcb725b2d31a4d14bf44ac346c9eb4b
giocalam-dev <giocalam@lam.fvg.it>
2026-05-23 22:51:51 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Network-first strategy - always get fresh content
-const CACHE_NAME = 'torneo-lam-v3';
+const CACHE_NAME = 'torneo-lam-v4';
 
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => {
